6a6b1aac9c488e2d46e9e8da950c1db18227225c
Gian <gian.defaria@hotmail.com>
2022-12-14 01:03:23 -0300

    adicionei o box com os valores que serão retornados pela api

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -14,6 +14,29 @@ const PartePrincipal = () => {
                     <img src={arrow} alt="arrow" className='arrow' />
                 </label>
                 
+                <section className='api--value--return'>
+
+                    <div className='ipp--box'>
+                        <h2>IP ADDRESS</h2>
+                        <p className='ip'></p>
+                    </div>
+
+                    <div className='location--box'>
+                        <h2>LOCATION</h2>
+                        <p className='location'></p>
+                    </div>
+
+                    <div className='timezone--box'>
+                        <h2>TIMEZONE</h2>
+                        <p className="timezone--box"></p>
+                    </div>
+
+                    <div className='isp--box'>
+                        <h2>ISP</h2>
+                        <p className="isp"></p>
+                    </div>
+
+                </section>
             </section>
             <section className='content--map'>
                 
