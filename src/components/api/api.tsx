cc110e867cea2f677968795c522d69986ff93d0b
Gian <gian.defaria@hotmail.com>
2022-12-15 02:46:08 -0300

    adicionei na variável location as informações do local do ip

--- src/components/api/api.tsx
+++ src/components/api/api.tsx
@@ -3,12 +3,17 @@
 function apiIp () {
 
     let ip = document.querySelector('.input--ip') as HTMLInputElement;
+    let location;
+    let timezone;
+    let isp;
     console.log(ip.value); 
 
     fetch('https://geo.ipify.org/api/v2/country,city?apiKey=at_m25176uh13PLKuNsDz5iswx3XGnHG&ipAddress=8.8.8.8').then((Response) =>{
         
         Response.json().then((data) => {
             console.log(data);
+            location = data.location.city + ' , ' + data.location.region + " " + data.location.postalCode;
+            console.log(location);
         })
 
     })
