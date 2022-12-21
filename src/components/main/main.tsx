057b0eb0ece1a36e71e17c65f7828f02d7ec301c
Gian <gian.defaria@hotmail.com>
2022-12-21 16:48:43 -0300

    adicionei bordar no mapa

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -164,7 +164,7 @@ function PartePrincipal() {
             <section className='content--map'>
                 {apiReturn && (
                     <div id='map'>
-                        <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={true} style={{height: "70vh"}}>
+                        <MapContainer center={[latitude, longitude]} zoom={13} scrollWheelZoom={true} style={{height: "70vh"}} >
                             <TileLayer
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
